15892cb1a9357e1b329b33d5afc8f11c4b4be43c
Ayman Ali <Aomara@sure.com.sa>
2024-06-24 12:18:18 +0300

    update mission repo

--- data/lib/features/mission/repository/mission_repository.dart
+++ data/lib/features/mission/repository/mission_repository.dart
@@ -9,15 +9,15 @@ class MissionRepository {
 
   Box<MissionModel> get missionBox => Hive.box("mission");
 
-  void addMission(MissionModel mission) {
-    missionBox.add(mission);
+  Future<void> addMission(MissionModel mission) async {
+    await missionBox.add(mission);
   }
 
-  void updateMission(MissionModel mission) {
-    missionBox.put(mission.id, mission);
+  Future<void> updateMission(MissionModel mission) async {
+    await missionBox.put(mission.id, mission);
   }
 
-  void deleteMission(MissionModel mission) {
-    missionBox.delete(mission.id);
+  Future<void> deleteMission(MissionModel mission) async {
+    await missionBox.delete(mission.id);
   }
 }
